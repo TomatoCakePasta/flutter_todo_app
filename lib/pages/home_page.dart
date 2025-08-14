7f2966da7f3efc8788a2c9308098de42eb950ed9
TomatoCakePasta <gaomuk50@gmail.com>
2025-08-14 19:55:24 +0900

    実装:チェックボックスの機能

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Laundry", false],
+    ["Grocery Shopping", false],
+  ];
+
+  // checkbox changed
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +32,15 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.yellow[700],
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(
-            taskName: "Make Tutorial",
-            taskCompleted: false,
-            onChanged: (p0) {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
